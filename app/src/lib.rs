8b4de92d9edb54cf3f102ba6404fb4edae116ecd
Pedro Nieto Méndez <pedro.nieto@sngular.com>
2024-11-24 19:14:58 +0100

    experiments

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -3,6 +3,7 @@ use crate::error_template::{AppError, ErrorTemplate};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use std::marker::PhantomData;
 
 pub mod error_template;
 
@@ -15,7 +16,7 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/start-axum-workspace.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+            <Title text="Welcome to Leptos"/>
 
         // content for this welcome page
         <Router fallback=|| {
@@ -32,15 +33,60 @@ pub fn App() -> impl IntoView {
     }
 }
 
+/// A progress bar that shows the progress of an operation.
+#[component]
+fn ProgressBar(
+    /// The maximum value of the progress bar.
+    #[prop(default = 100)]
+    max: u16,
+    /// How much progress should be displayed.
+    #[prop(into)]
+    progress: Signal<i32>,
+) -> impl IntoView {
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
+
+        <br/>
+    }
+}
+
+#[component]
+fn ProgressBarOpt(#[prop(optional)] progress: Option<Box<dyn Fn() -> i32>>) -> impl IntoView {
+    progress.map(|progress| {
+        view! {
+            <progress
+                max=100
+                value=progress
+            />
+            <br/>
+        }
+    })
+}
+
+#[component]
+fn SizeOf<T: Sized>(#[prop(optional)] _ty: PhantomData<T>) -> impl IntoView {
+    std::mem::size_of::<T>()
+}
+
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let double_count = move || count.get() * 2;
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <button on:click=move |_| { set_count.update(|n| *n += 1); }>
+            "Click me"
+        </button>
+        <ProgressBar progress=count />
+        <ProgressBar progress=Signal::derive(double_count) />
+        <SizeOf<usize>/>
+        <SizeOf<String >/>
+        <ProgressBarOpt progress=Box::new(double_count) />
+
+        // add a second progress bar
     }
 }
